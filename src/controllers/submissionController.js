da8cc4eb93d102585924200942a5d9701bdb746c
Himansh <himanshbansal200521@gmail.com>
2025-08-24 10:41:56 +0530

    FIX: bug fixes related to repo and use of send method instead of json in fastify

--- src/controllers/submissionController.js
+++ src/controllers/submissionController.js
@@ -1,7 +1,7 @@
 //  validation can be added
 async function createSubmission(req, res){
     const response  = await this.submissionService.addSubmission(req.body);
-    return res.status(201).json({
+    return res.status(201).send({
         error: {},
         data: response,
         success: true,
