624926e00881c488cdc2d97d3f2919b6f5682126
Himansh <himanshbansal200521@gmail.com>
2025-08-25 19:01:14 +0530

    ADD: added sync communication btw microservices

--- src/services/submissionService.js
+++ src/services/submissionService.js
@@ -1,3 +1,4 @@
+const { fetchProblemDetails } = require('../apis/problemAdminApi');
 const SubmissionProducer = require('../producers/submissionQueueProducer')
 
 class SubmissionService {
@@ -6,13 +7,33 @@ class SubmissionService {
     }
 
     async addSubmission(submissionPayload){
+
+        const problemId = submissionPayload.problemId;
+
+        const problemAdminApiResponse = await fetchProblemDetails(problemId);
+        if(!problemAdminApiResponse ) {
+            throw {message: "Not able to fetch problem details"};
+        }
+
+        const languageCodeStub = problemAdminApiResponse.data.codeStubs.find(codeStub => codeStub.language.toLowerCase() === submissionPayload.language.toLowerCase());
+        console.log(languageCodeStub);
+
+        submissionPayload.code = languageCodeStub.startSnippet + "\n\n" + submissionPayload.code + "\n\n" + languageCodeStub.endSnippet;
+
         const submission = await this.submissionRepository.createSubmission(submissionPayload);
         if(!submission){
             //here error handling can be better
             throw {message: "Not able to create submission"};
         }
         console.log(submission);
-        const response = await SubmissionProducer(submission)
+        const response = await SubmissionProducer({
+            [submission._id]: {
+                code: submission.code,
+                language: submission.language,
+                inputCase: problemAdminApiResponse.data.testCases[0].input,
+                outputCase: problemAdminApiResponse.data.testCases[0].output,
+            }
+        });
         return {queueResponse: response, submission};
     }
 }
